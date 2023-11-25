54f1be5ae69e355a002b3afd6050513ad629c1a1
ying <huaiy1105@gmail.com>
2023-11-25 11:48:19 +0000

    install socket package to client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ChatRoom from './assets/component/ChatRoom'
 
 function App() {
-  return {
+  return (
     <ChatRoom/>
-  }
+  )
 }
 
 export default App
